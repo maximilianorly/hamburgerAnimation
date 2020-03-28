b117d014613c09d2661e56a31c973b55e2310df8
Maximilian Riley <maximilian@Maximilians-MacBook-Pro.local>
2020-03-28 01:08:58 +0000

    edited deploy script

--- scripts/hamburger-animation-deploy.js
+++ scripts/hamburger-animation-deploy.js
@@ -3,19 +3,19 @@ const fs = require("fs");
 
 (async () => {
   try {
-    await execa("git", ["checkout", "--orphan", "gh-pages"]);
+    await execa("git", ["checkout", "--orphan", "hamburger-animation"]);
     console.log("Building...");
     await execa("npm", ["run", "build"]);
     // Understand if it's dist or build folder
     const folderName = fs.existsSync("dist") ? "dist" : "build";
     await execa("git", ["--work-tree", folderName, "add", "--all"]);
-    await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
-    console.log("Pushing to gh-pages...");
-    await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
+    await execa("git", ["--work-tree", folderName, "commit", "-m", "hamburger-animation"]);
+    console.log("Pushing to hamburger-animation...");
+    await execa("git", ["push", "origin", "HEAD:hamburger-animation", "--force"]);
     await execa("rm", ["-r", folderName]);
     await execa("git", ["checkout", "-f", "master"]);
-    await execa("git", ["branch", "-D", "gh-pages"]);
-    console.log("Successfully deployed");
+    await execa("git", ["branch", "-D", "hamburger-animation"]);
+    console.log("Successfully deployed!");
   } catch (e) {
     console.log(e.message);
     process.exit(1);
