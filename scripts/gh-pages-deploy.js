c66f9bf6a967a98b50095ee4f221adf15b7b04c7
Maximilian Riley <maximilian@Maximilians-MacBook-Pro.local>
2020-03-28 17:42:05 +0000

    added emoji json, updated readme.md and updated deploy script

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -1,24 +1,26 @@
+// Refer to line 32 of README.md
+
 const execa = require("execa");
 const fs = require("fs");
 const emoji = require("node-emoji");
 
 (async () => {
   try {
-    console.log(`Grab a ${emoji.get('coffee')} while we build and deploy... ${emoji.get('computer') + emoji.get('rocket')}`)
+    console.log(`Grab a ${emoji.get('coffee')}   while we build and deploy... ${emoji.get('computer')} ${emoji.get('rocket')}`)
     await execa("git", ["checkout", "--orphan", "gh-pages"]);
-    console.log(`Building... ${emoji.get('building_construction' + emoji.get('construction'))}`);
+    console.log(`Building... ${emoji.get('building_construction')} ${emoji.get('construction')}`);
     await execa("npm", ["run", "build"]);
     // Understand if it's dist or build folder
     const folderName = fs.existsSync("dist") ? "dist" : "build";
     await execa("git", ["--work-tree", folderName, "add", "--all"]);
     await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
-    console.log(`Pushing to gh-pages... ${emoji.get('arrow_upper_right') + emoji.get('cloud')}`);
+    console.log(`Pushing to gh-pages... ${emoji.get('arrow_upper_right')} ${emoji.get('cloud')}`);
     await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
     await execa("rm", ["-r", folderName]);
     await execa("git", ["checkout", "-f", "master"]);
     await execa("git", ["branch", "-D", "gh-pages"]);
-    console.log(`${emoji.get('tada') + emoji.get('tada') + emoji.get('tada')}`)
-    console.log(`Successfully deployed! ${emoji.get('postbox') + emoji.get('person_in_lotus_position')}`);
+    console.log(`${emoji.get('tada')} ${emoji.get('tada')} ${emoji.get('tada')}`)
+    console.log(`Successfully deployed! ${emoji.get('postbox')} ${emoji.get('person_in_lotus_position')}`);
   } catch (e) {
     console.log(e.message);
     process.exit(1);
